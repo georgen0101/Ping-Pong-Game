0d904d90fb6fa7d985035734c1e8c6b65894fd6a
George N <93057254+georgen0101@users.noreply.github.com>
2022-09-05 10:16:06 -0500

    Proyecto Final

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
-"""
-This is the Ping-Pong game not completed yet only the variables and classes.
-For the program to do something instead of just a black screen.
-I add a temporary code from the Turtle graphics library.
-"""
-
 # Libraries
 # from turtle import Screen
-from scoreboard import Scoreboard
-from ball import Ball
+from turtle import Screen
 from paddle import Paddle
-# Temporary import, * imports all.
-from turtle import *
-
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
-screen.title("Ping Pong")
+screen.title("Pong Game")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 screen.listen()
 
-# r_paddle = Paddle((350, 0))
-# l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 ball = Ball()
 scoreboard = Scoreboard()
 
-# screen.onkeypress(r_paddle.move_up, "Up")
-# screen.onkeypress(r_paddle.move_down, "Down")
-# screen.onkeypress(l_paddle.move_up, "w")
-# screen.onkeypress(l_paddle.move_down, "s")
+screen.onkeypress(r_paddle.move_up, "Up")
+screen.onkeypress(r_paddle.move_down, "Down")
+screen.onkeypress(l_paddle.move_up, "w")
+screen.onkeypress(l_paddle.move_down, "s")
 
-# --------- Temporary code ---------
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+
+game_is_on = True
+
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_points()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_points()
 
 
 screen.exitonclick()
